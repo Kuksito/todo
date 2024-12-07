781a53a3fd005030acc9a64d10b170dc3107097f
karina <123593292+Kuksito@users.noreply.github.com>
2024-12-07 12:44:34 +0300

    Add logic for appending tasks in lists (folders), doesn't work properly (append tasks only to the last folder with creating an empty task) localstorage does not work

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 import './styles.css';
 
-const content = document.querySelector('#content');
 const addTaskBtn = document.querySelector('#btn-add-task');
 const addListBtn = document.querySelector('#btn-add-list');
 const dialog = document.querySelector('[data-dialog]');
 const form = document.querySelector('#task-form');
-const formListBtn = document.querySelector('#list');
 const taskContainer = document.querySelector('#task-container');
 const listContainer = document.querySelector('#list-container');
-
-
-// const taskTitle = document.querySelector('#task-title');
-// const taskPriority = document.querySelector('#task-priority');
-// const taskDueDate = document.querySelector('#task-date');
-// const btnCheck = document.querySelector('#task-btn-check');
-// const btnDelete = document.querySelector('#task-delete-btn');
-// const taskDescr = document.querySelector('#task-description');
+const listSelect = document.querySelector('#list');
+const listDialog = document.querySelector('#list-dialog');
+const listForm = document.querySelector('#list-form');
 
 const tasks = [];
 const lists = [];
 let newTask;
-let newList;
+let listInfo;
 
 class Task{
     constructor(title, description, dueDate, priority, list){
@@ -35,20 +28,9 @@ class Task{
     }
 };
 
-// function createTaskInfo(task){
-//     taskTitle.innerHTML = `${task.title}`;
-//     taskPriority.textContent = `${task.priority}`;
-//     taskDueDate.textContent = `due to: ${task.dueDate}`;
-//     taskDescr.textContent = `${task.description}`;
-//     // listInfo.textContent = `${task.list}`;
-//     btnDelete.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 26 26">
-// <path d="M 11.5 -0.03125 C 9.542969 -0.03125 7.96875 1.59375 7.96875 3.5625 L 7.96875 4 L 4 4 C 3.449219 4 3 4.449219 3 5 L 3 6 L 2 6 L 2 8 L 4 8 L 4 23 C 4 24.644531 5.355469 26 7 26 L 19 26 C 20.644531 26 22 24.644531 22 23 L 22 8 L 24 8 L 24 6 L 23 6 L 23 5 C 23 4.449219 22.550781 4 22 4 L 18.03125 4 L 18.03125 3.5625 C 18.03125 1.59375 16.457031 -0.03125 14.5 -0.03125 Z M 11.5 2.03125 L 14.5 2.03125 C 15.304688 2.03125 15.96875 2.6875 15.96875 3.5625 L 15.96875 4 L 10.03125 4 L 10.03125 3.5625 C 10.03125 2.6875 10.695313 2.03125 11.5 2.03125 Z M 6 8 L 11.125 8 C 11.25 8.011719 11.371094 8.03125 11.5 8.03125 L 14.5 8.03125 C 14.628906 8.03125 14.75 8.011719 14.875 8 L 20 8 L 20 23 C 20 23.5625 19.5625 24 19 24 L 7 24 C 6.4375 24 6 23.5625 6 23 Z M 8 10 L 8 22 L 10 22 L 10 10 Z M 12 10 L 12 22 L 14 22 L 14 10 Z M 16 10 L 16 22 L 18 22 L 18 10 Z"></path>
-// </svg>`;
-//     taskPriority.style.paddingRight = '5px';
-
-//     console.log(taskTitle)
-//     console.log(taskPriority)
-// }
+addTaskBtn.addEventListener('click', () => {
+    dialog.showModal();
+});
 
 function createTaskInfo(task){
     const taskWrapper = document.createElement('div');
@@ -77,7 +59,6 @@ function createTaskInfo(task){
     taskPriority.textContent = `${task.priority}`;
     taskDueDate.textContent = `due to: ${task.dueDate}`;
     taskDescr.textContent = `${task.description}`;
-    // listInfo.textContent = `${task.list}`;
     btnDelete.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 26 26">
 <path d="M 11.5 -0.03125 C 9.542969 -0.03125 7.96875 1.59375 7.96875 3.5625 L 7.96875 4 L 4 4 C 3.449219 4 3 4.449219 3 5 L 3 6 L 2 6 L 2 8 L 4 8 L 4 23 C 4 24.644531 5.355469 26 7 26 L 19 26 C 20.644531 26 22 24.644531 22 23 L 22 8 L 24 8 L 24 6 L 23 6 L 23 5 C 23 4.449219 22.550781 4 22 4 L 18.03125 4 L 18.03125 3.5625 C 18.03125 1.59375 16.457031 -0.03125 14.5 -0.03125 Z M 11.5 2.03125 L 14.5 2.03125 C 15.304688 2.03125 15.96875 2.6875 15.96875 3.5625 L 15.96875 4 L 10.03125 4 L 10.03125 3.5625 C 10.03125 2.6875 10.695313 2.03125 11.5 2.03125 Z M 6 8 L 11.125 8 C 11.25 8.011719 11.371094 8.03125 11.5 8.03125 L 14.5 8.03125 C 14.628906 8.03125 14.75 8.011719 14.875 8 L 20 8 L 20 23 C 20 23.5625 19.5625 24 19 24 L 7 24 C 6.4375 24 6 23.5625 6 23 Z M 8 10 L 8 22 L 10 22 L 10 10 Z M 12 10 L 12 22 L 14 22 L 14 10 Z M 16 10 L 16 22 L 18 22 L 18 10 Z"></path>
 </svg>`;
@@ -89,16 +70,12 @@ function createTaskInfo(task){
     taskTitleWrapper.append(taskTitle, taskDueDate);
     taskTitle.prepend(taskPriority);
     taskBtnsContainer.append(btnCheck, btnDelete);
-    taskContainer.append(taskWrapper);
 
     generatePriorityColor(taskPriority);
     generateCheckBtn(btnCheck, taskTitleWrapper);
     openCloseDescription(taskTitleWrapper, taskDescr);
-
-    return{ taskPriority };
-}
-
-// let taskInfo = createTaskInfo();
+    deleteTask(btnDelete, taskWrapper);
+};
 
 function generatePriorityColor(priority){
     if(priority.textContent === '1'){
@@ -126,6 +103,13 @@ function generateCheckBtn(btnCheck, taskTitleWrapper){
     });
 };
 
+function deleteTask(deleteBtn, item){
+    deleteBtn.addEventListener('click', () => {
+        item.remove();
+        tasks.splice(item);    
+        console.log(tasks);
+    });
+};
 
 function openCloseDescription(taskTitleWrapper, descr){
     taskTitleWrapper.addEventListener('click', () => {
@@ -137,54 +121,18 @@ function openCloseDescription(taskTitleWrapper, descr){
     });
 };
 
-// function createTaskInfo(task){
-    
-//     // const listInfo = document.createElement('p');
-//     item.classList.add('item');
-//     itemInfo.classList.add('item-info');
-//     itemTitleWrapper.classList.add('title-wrapper');
-//     itemTitle.classList.add('item-title');
-//     itemPriority.classList.add('priority');
-//     itemDueDate.classList.add('date');
-//     itemBtnsContainer.classList.add('item-btns');
-//     btnCheck.classList.add('btn', 'btn-check');
-//     btnDelete.classList.add('btn');
-//     descr.classList.add('description');
-    
-//     item.append(itemInfo, descr);
-//     itemInfo.append(itemTitleWrapper, itemBtnsContainer);
-//     itemTitleWrapper.append(itemTitle, itemDueDate);
-//     itemTitle.prepend(itemPriority);
-//     itemBtnsContainer.append(btnCheck,btnDelete);
-//     taskContainer.append(item);
+function addTask(){
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        newTask = new Task(title.value, description.value, dueDate.value, priority.value);
+        tasks.push(newTask);
+        dialog.close();
+        form.reset();
+        displayTask();        
+    });
+};
 
-//     if(formListBtn.textContent != ''){
-//         item.append(itemInfo, descr);
-//         itemInfo.append(itemTitleWrapper, itemBtnsContainer);
-//         itemTitleWrapper.append(itemTitle, itemDueDate);
-//         itemTitle.prepend(itemPriority);
-//         itemBtnsContainer.append(btnCheck,btnDelete);
-//         taskContainer.append(item);
-//     }
-
-//     return{item, descr, itemPriority, btnCheck}
-// };
-
-// function openCloseDescription(descr){
-//     descr.addEventListener('click', () => {
-//         let taskDesk;
-//         for(let i = 0; i < tasks.length; i++){
-//             taskDesk = createTaskInfo(tasks[i]);
-//         }
-//         // PROBLEM!!! need to pass an argument but i don't have it??
-//         // NOW IT OPENS THE DESCR BUT DOESN'T CLOSE IT AND ADDS THE SAME DIVS AGAIN
-//         if(taskDesk.descr.style.display === 'block'){
-//             taskDesk.descr.style.display = 'none';
-//         } else {
-//             taskDesk.descr.style.display = 'block';
-//         }
-//     });
-// };
+//addTask();
 
 function displayTask(){
     taskContainer.innerHTML = '';
@@ -193,97 +141,135 @@ function displayTask(){
     };
 };
 
-function addTask(){
+function createListInfo(){
+    const listWrapper = document.createElement('div');
+    const item = document.createElement('div');
+    const taskTitle = document.createElement('h4');
+    const taskPriority = document.createElement('span');
+    const taskBtnsContainer = document.createElement('div');
+    const btnCheck = document.createElement('button');
+    const btnDelete = document.createElement('button');
+    const descr = document.createElement('p');
+    const listTitle = document.querySelector('#list-title');
+    const newList = document.createElement('p');
+    newList.innerHTML = listTitle.value;
+    listWrapper.setAttribute('id',`${newList.textContent}`);
+    listWrapper.classList.add('list-wrapper');
+    listTitle.classList.add('list-title');
+    item.classList.add('item');
+    taskTitle.classList.add('item-title');
+    taskPriority.classList.add('priority');
+    taskBtnsContainer.classList.add('item-btns', 'item-btn-small');
+    btnCheck.classList.add('btn', 'btn-check', 'btn-small');
+    btnDelete.classList.add('btn', 'btn-small');
+    listContainer.append(listWrapper);
+    listWrapper.append(newList, item);
+
+    lists.push(newList.textContent);
+    console.log(listWrapper.id)
+
+    return function displayTaskInAList(newTask){
+        item.append(newTask)
+    };
+};
+
+addListBtn.addEventListener('click', ()=>{
+    listDialog.showModal();
+});
+
+listForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    listInfo = createListInfo();
+    updateListOptions();
+    listDialog.close();
+    listForm.reset();
+});
+
+function updateListOptions(){
+    const option = document.createElement('option');
+    for(let i = 0; i < lists.length; i++){
+        option.value = lists[i];
+        option.textContent = lists[i];
+        option.id = i;
+        listSelect.append(option);
+    };
+};
+
+listSelect.addEventListener('click', getSelectedValue);
+
+function getSelectedValue(){
+    const selected = listSelect.value;
+    if(lists.includes(selected)){
+        appendTaskToList()
+    } else {
+        addTask()
+    }
+};
+
+const a = [];
+
+function appendTaskToList(){
     form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        newTask = new Task(title.value, description.value, dueDate.value, priority.value);
-        tasks.push(newTask);
-        dialog.close();
-        form.reset();
-        formListBtn.value = '';
-        displayTask();
+        const savedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    e.preventDefault();
+    const taskTitle = document.getElementById('title');
+    const taskDescr = document.getElementById('description');
+    const taskDueDate = document.getElementById('dueDate');
+    const taskPriority = document.getElementById('priority');
+
+    const newTask = document.createElement('div');
+    newTask.classList.add('item-info');
+    newTask.innerHTML = `
+    <h4 class="item-title" id="task-title"><span class="priority" id="task-priority">${taskPriority.value}</span>${taskTitle.value}</h4>
+    <p class="date" id="task-date">${taskDueDate.value}</p>
+    <div class="item-btns">
+        <button class="btn btn-check" id="task-btn-check"></button>
+        <button class="btn" id="task-delete-btn"></button>
+    </div>
+    <p class="description" id="task-description">${taskDescr.value}</p>`
+
+    // APPEND NEW TASK IN THE LAST LIST!!! WITH EMPTY DIV AFTER
+    listInfo(newTask);
+    saveTaskToLocalStorage(newTask);
+    localStorage.setItem('tasks', JSON.stringify(savedTasks))
+    dialog.close();
+    form.reset();
     });
 };
 
-addTask();
-
-addTaskBtn.addEventListener('click', () => {
-    dialog.showModal();
-});
-
-class List{
-    constructor(listTitle){
-        this.listTitle = listTitle;
-    }
-
-    displayList(list){
-        const listWrapper = document.createElement('div');
-        const listTitle = document.createElement('h3');
-        const item = document.createElement('div');
-        const itemTitle = document.createElement('h4');
-        const itemPriority = document.createElement('span');
-        const itemBtnsContainer = document.createElement('div');
-        const btnCheck = document.createElement('button');
-        const btnDelete = document.createElement('button');
-        listWrapper.classList.add('list-wrapper');
-        listTitle.classList.add('list-title');
-        item.classList.add('item');
-        itemTitle.classList.add('item-title');
-        itemPriority.classList.add('priority');
-        itemBtnsContainer.classList.add('item-btns', 'item-btn-small');
-        btnCheck.classList.add('btn', 'btn-check', 'btn-small');
-        btnDelete.classList.add('btn', 'btn-small');
-        listTitle.textContent = `${list.listTitle}`;
-        listContainer.append(listWrapper);
-        listWrapper.append(listTitle, item);
-    }
+function saveTaskToLocalStorage(task){
+    const savedTasks = [];
+    savedTasks.push(task.textContent);
+    const uniqueTasks = [...new Set(savedTasks)]
+    localStorage.setItem('savedTasks', JSON.stringify(uniqueTasks));
+    console.log(uniqueTasks)
 };
 
-function addList(){
-    listContainer.innerHTML = '';
-    for(let i = 0; i < lists.length; i++){
-        newList.displayList(lists[i]);
-    };
-};
+// does not work!!!
+function getTasksFromLocalStorage(){
+    const savedTasks = JSON.parse(localStorage.getItem('savedTasks')) || [];
+    savedTasks.forEach(task => {
+        const taskTitle = document.getElementById('title');
+        const taskDescr = document.getElementById('description');
+        const taskDueDate = document.getElementById('dueDate');
+        const taskPriority = document.getElementById('priority');
 
-addList();
+        const newTask = document.createElement('div');
+        newTask.classList.add('item-info');
+        newTask.innerHTML = `
+        <h4 class="item-title" id="task-title"><span class="priority" id="task-priority">${taskPriority.value}</span>${taskTitle.value}</h4>
+        <p class="date" id="task-date">${taskDueDate.value}</p>
+        <div class="item-btns">
+            <button class="btn btn-check" id="task-btn-check"></button>
+            <button class="btn" id="task-delete-btn"></button>
+        </div>
+        <p class="description" id="task-description">${taskDescr.value}</p>`
 
-addListBtn.addEventListener('click', () => {
-    const titleInput = document.createElement('input');
-    const titleLabel = document.createElement('label');
-    const inputBtn = document.createElement('button');
-    inputBtn.classList.add('btn')
-    titleInput.type = 'text';
-    titleInput.id = 'listTitle';
-    titleLabel.textContent = 'Enter list title';
-    inputBtn.innerHTML = `<svg width="18px" height="13px" viewBox="0 0 18 13" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title>check</title><desc>Created with Sketch.</desc><g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="Rounded" transform="translate(-171.000000, -3438.000000)"><g id="Navigation" transform="translate(100.000000, 3378.000000)"><g id="-Round-/-Navigation-/-check" transform="translate(68.000000, 54.000000)"><g><polygon id="Path" points="0 0 24 0 24 24 0 24"></polygon><path d="M9,16.17 L5.53,12.7 C5.14,12.31 4.51,12.31 4.12,12.7 C3.73,13.09 3.73,13.72 4.12,14.11 L8.3,18.29 C8.69,18.68 9.32,18.68 9.71,18.29 L20.29,7.71 C20.68,7.32 20.68,6.69 20.29,6.3 C19.9,5.91 19.27,5.91 18.88,6.3 L9,16.17 Z" id="🔹-Icon-Color" fill="#1D1D1D"></path></g></g></g></g></g></svg>`;
-    titleLabel.setAttribute('for', 'listTitle');
-    listContainer.prepend(titleLabel, titleInput, inputBtn);
-
-    inputBtn.addEventListener('click', () => {
-        newList = new List(listTitle.value);
-        lists.push(newList);
-        addList();
+        // APPEND NEW TASK IN THE LAST LIST!!! WITH EMPTY DIV AFTER
+        listInfo(newTask);
     });
-});
-
-function createListDivs(){
-    const recordOfList = document.createElement('div');
-    recordOfList.classList.add('list-choose-wrapper');
-    form.append(recordOfList);
-    for(let i = 0; i < lists.length; i++){
-        const recordDiv = document.createElement('div');
-        recordDiv.classList.add('list-choose-div');
-        recordDiv.append(lists[i].listTitle);
-        recordOfList.append(recordDiv);
-
-        recordDiv.addEventListener('click', () => {
-            recordOfList.style.display = 'none';
-            // formListBtn.textContent = recordDiv.textContent;  
-            formListBtn.value = recordDiv.textContent; 
-        });
-    };
 };
 
-
-formListBtn.addEventListener('click', createListDivs);
+document.addEventListener('DOMContentLoaded', () => {
+    getTasksFromLocalStorage();
+})
